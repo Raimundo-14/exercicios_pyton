58365a40e6aa748a0c3746131a4c7d1db5f6dfb4
Raimundo Albuquerque <102672431+Raimundo-14@users.noreply.github.com>
2023-10-28 16:14:28 -0300

    ex de 1 a 7

---
+++ ex_06.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu primeiro nome: ').title()
+sobrenome = input('Digite seu sobrenome: ').title()
+nome_completo = nome + ' ' + sobrenome
+
+print(nome_completo)
---
+++ ex_07.py
@@ -0,0 +1,8 @@
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite um número: '))
+num3 = float(input('Digite um número: '))
+soma = num1 + num2 + num3
+media = soma/3
+# round(media)
+print(
+    f'A soma de {num1}, {num2} e {num3} é igual a {soma}e sua media é igual a {media:.2f}')
